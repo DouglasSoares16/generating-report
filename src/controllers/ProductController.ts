89e173d6ac609e138ab3f85a5a95b9c5fa4bda67
Douglas Soares <douglasmaismcpe@gmail.com>
2022-06-12 07:52:22 -0300

    Product Created

---
+++ src/controllers/ProductController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { getCustomRepository, getRepository, Repository } from "typeorm";
+import { Product } from "../entities/Product";
+import { ProductRepository } from "../repositories/ProductRepository";
+
+class ProductController {
+  async create(req: Request, res: Response) {
+    const repository = getCustomRepository(ProductRepository);
+    const { name, price, description } = req.body;
+
+    const product = repository.create({ name, price, description });
+
+    await repository.save(product);
+
+    return res.status(201).json({
+      message: "Created Product"
+    });
+  }
+
+  async list(req: Request, res: Response) {
+    const repository = getCustomRepository(ProductRepository);
+    const products = await repository.find();
+
+    return res.json(products);
+  }
+}
+
+export { ProductController };
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { ProductController } from "./controllers/ProductController";
+
+const productController = new ProductController();
 
 const routes = Router();
+
+routes.get("/products", productController.list);
+routes.post("/products", productController.create);
 
 export { routes };
